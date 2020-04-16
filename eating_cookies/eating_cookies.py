e64e66bbc38ec9a60f59877e8da91562ebff9638
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-04-16 13:55:26 -0400

    eating_cookies.py: write examples

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,10 +2,77 @@
 
 import sys
 
+########################################
+# EXAMPLES
+#---------------------------------------
+# eating_cookies(0) => 1
+# - 0
+# -- stop (*)
+#---------------------------------------
+# eating_cookies(1) => 1
+# - 1
+# -- stop (*)
+#---------------------------------------
+# eating_cookies(2) => 2
+# - 1 1
+# -- 2
+# --- stop (*)
+#---------------------------------------
+# eating_cookies(3) => 4
+# - 1 1 1
+# -- 2 1
+# --- 3
+# ---- stop (*)
+# -- 1 2
+# --- 3 (*)
+#---------------------------------------
+# eating_cookies(4) => 7
+# - 1 1 1 1
+# -- 2 1 1
+# --- 3 1
+# ---- stop (*)
+# --- 2 2
+# ---- stop (*)
+# -- 1 2 1
+# --- 3 1 (*)
+# --- 1 3
+# ---- stop (*)
+# -- 1 1 2
+# --- 2 2 (*)
+# --- 1 3 (*)
+#---------------------------------------
+# eating_cookies(5) => 13
+# - 1 1 1 1 1
+# -- 2 1 1 1
+# --- 3 1 1
+# ---- 3 2
+# ----- stop (*)
+# --- 2 2 1
+# ---- 2 3
+# ----- stop (*)
+# --- 2 1 2
+# ---- 3 2 (*)
+# ---- 2 3 (*)
+# -- 1 2 1 1
+# --- 3 1 1 (*)
+# --- 1 3 1
+# ---- stop (*)
+# --- 1 2 2
+# ---- 3 2 (*)
+# -- 1 1 2 1
+# --- 2 2 1 (*)
+# --- 1 3 1 (*)
+# --- 1 1 3
+# ---- 2 3 (*)
+# -- 1 1 1 2
+# --- 2 1 2 (*)
+# --- 1 2 2 (*)
+# --- 1 1 3 (*)
+########################################
 
-# The cache parameter is here for if you want to implement
-# a solution that is more efficient than the naive
-# recursive solution
+
+# The cache parameter is here for if you want to implement a solution that is more
+# efficient than the naive recursive solution.
 def eating_cookies(n, cache=None):
     pass
 
