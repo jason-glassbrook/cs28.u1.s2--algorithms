36da8e310df354f452fee874b92f1d66f90955bf
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-04-16 18:42:25 -0400

    makeing_change.py: write some thoughts

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,6 +2,52 @@
 
 import sys
 
+########################################
+# THINKING
+#---------------------------------------
+# We want to find the number of _combinations_ possible when making change
+# from some amount with some list of denominations.
+#
+# We are finding the number of _combinations_, not _permutations_,
+# so order does not matter:
+#
+#     make_change(5, [1, 2, 3])
+#
+#     1. 3 2 <-> 2 3
+#     2. 3 1 1 <-> 1 3 1 <-> 1 1 3
+#     3. 2 2 1 <-> 2 1 2 <-> 1 2 2
+#     4. 2 1 1 1 <-> 1 2 1 1 <-> 1 1 2 1 <-> 1 1 1 2
+#     5. 1 1 1 1 1
+#
+#     => 5 ways # ... not 13
+#
+# Because order does not matter, we can sequentially divide the amount
+# into the largest possible denominations, and then divide each of those
+# recursively...
+#
+#     make_change(5, [1, 2, 3])
+#
+#     5 -> 3 2 or 2 2 1 or 1 1 1 1 1 (1 + 1 + 1)
+#     [base] 3 -> 2 1 or 1 1 1 (2)
+#     [base] 2 -> 1 1 (1)
+#
+#     => 5 ways
+#
+#     make_change(7, [1, 2, 3])
+#
+#     7 -> 3 3 1 or 2 2 2 1 or 1 1 1 1 1
+#     [base] 3 -> 2 1 or 1 1 1 (2)
+#     [base] 2 -> 1 1 (1)
+#
+#     => 5 ways
+#
+# We need to generate the base cases, because we do not know the
+# denominations beforehand...
+#
+#     coins = [1, 5, 10, 25, 50]
+#
+########################################
+
 
 def making_change(amount, denominations):
     pass
